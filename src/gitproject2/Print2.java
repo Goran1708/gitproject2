e488ec53ffdfec328c71670dc9b719d6781a60bf
Goran <gorancolovic10@gmail.com>
2013-11-26 12:04:11 +0100

    initial commit
    initial

---
+++ src/gitproject2/Print2.java
@@ -0,0 +1,20 @@
+package gitproject2;
+
+
+class Sent{
+	
+	public Sent(){
+		
+		System.out.println("Hello again");
+		System.out.println("Hello once again");
+		System.out.println("Hello once again!!!");
+	}
+}
+
+public class Print2 {
+
+	public static void main(String[] args){
+		
+		new Sent();
+	}
+}
